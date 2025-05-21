72ea3e643afa4d4864a862b81f034d12de324f1b
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-05-21 04:40:46 -0700

    .

--- AutomatedTellerMachineTest.java
+++ AutomatedTellerMachineTest.java
@@ -1,28 +1,31 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AutomatedTellerMachineTest {
+	AutomatedTellerMachine atm = new AutomatedTellerMachine(); 
+	private Account[] accounts = {new Account("john", "0123456789", 50.00), new Account("james", "9876543210", 50000.00), new Account("Bolaji", "9786543210", 500.00) };
+
+
+	@BeforeEach
+	public void setUp() {
+	atm.setAccounts(accounts);
+
+}
 
 	@Test
 	public void testCanWithdrawFunds() {
-	//pre-condition(s)
-
-	Account johnsAccount = new Account("john", "0123456789", 50.00);
-	Account jamesAccount = new Account("james", "9876543210", 50000.00);
-	Account bolajiAccount = new Account("Bolaji", "9786543210", 500.00);
+		atm.withdraw(accounts[1].getAccountNumber(), 20000);	
+		System.out.println(accounts[1]);
+		assertEquals(30000, accounts[1].getBalance());
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+}
 	
-	Account[] accounts = {johnsAccount, jamesAccount, bolajiAccount};
 
-	AutomatedTellerMachine atm = new AutomatedTellerMachine();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-
-	atm.setAccounts(accounts);
-
-	//action
-	atm.withdraw(jamesAccount.getAccountNumber(), 20000);
-
-	//check(s)
-	assertEquals(30000, jamesAccount.getBalance());
-
+	@Test
+	public void testWithdrawNegativeAmountThrowsException(){
+		atm.withdraw(account[1].getAccountNumber(), -30000);
+		assertThrows();
 
 
 
